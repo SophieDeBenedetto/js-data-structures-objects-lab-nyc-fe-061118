fdd523f35368db9cceb6f92b33ed24c93530c0d0
Sophie DeBenedetto <sophie.debenedetto@gmail.com>
2018-07-25 13:23:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 let driver = {};
 
+let driver = {};
+
+function updateDriverWithKeyAndValue(driver,key,value) {
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function updateDriverWithKeyAndValue(driver, key, value) {
-  return {...driver, [key]: value}
+
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-  driver[key] = value
-  return driver
+  
 }
 
 function deleteFromDriverByKey(driver, key) {
-  let copyOfDriver = {...driver}
-  delete copyOfDriver[key]
-  return copyOfDriver
+  
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
-  delete driver[key]
-  return driver
+  
 }
 
 
